0af7df7c2e678d4e5c7f213bf9a7c4015460d88b
julieperbal <julie.perbal@gmail.com>
2017-04-13 16:29:58 +0200

    ajout techno Java

---
+++ Java/Workplace/Test1/src/firstClass.java
@@ -0,0 +1,11 @@
+
+public class firstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Word ! \n");
+		System.out.println("My name is");
+		System.out.print("cysboy");
+	}
+
+}
